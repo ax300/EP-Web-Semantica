64143b080e39a51c02e6ee5ec592888aea432fef
MiKalec <msmikaelis@gmail.com>
2019-12-12 00:08:30 -0300

    spring project

--- src/main/java/controller/InterfaceController.java
+++ src/main/java/controller/InterfaceController.java
@@ -3,16 +3,18 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InterfaceController {
     @GetMapping("/home")
-    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    public String home() {
+        return "home";
     }
 
-
+    @GetMapping("/lojas")
+    public String lojas(Model model) {
+//        model.addAllAttributes( /*TODO metodo que pega as lojas*/ );
+        return "lojas";
+    }
 
 }
